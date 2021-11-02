18b68e54df57dd79daf0b19ce1ae027dafbffc82
Ben van de Sande <ben@pomodoro.nl>
2021-11-02 16:53:03 +0100

    update cmp layout

--- lua/plugins/configs/cmp.lua
+++ lua/plugins/configs/cmp.lua
@@ -8,7 +8,17 @@ vim.opt.completeopt = "menuone,noselect"
 
 -- nvim-cmp setup
 cmp.setup {
-   snippet = {
+  max_abbr_width = 100,
+  max_kind_width = 100,
+  max_menu_width = 100,
+  documentation = {
+    border = "rounded",
+    max_width = 120,
+    min_width = 60,
+    max_height = math.floor(vim.o.lines * 0.3),
+    min_height = 1,
+  },
+  snippet = {
       expand = function(args)
          require("luasnip").lsp_expand(args.body)
       end,
