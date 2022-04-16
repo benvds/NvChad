52b84d7ba1cfedb15c8bb143630a4ca9a27c7a2a
Ben van de Sande <ben@pomodoro.nl>
2022-04-16 16:12:52 +0200

    fix eslint, remove null-ls

--- lua/user/lsp/lsp-installer.lua
+++ lua/user/lsp/lsp-installer.lua
@@ -6,9 +6,13 @@ end
 -- Register a handler that will be called for all installed servers.
 -- Alternatively, you may also register handlers on specific server instances instead (see example below).
 lsp_installer.on_server_ready(function(server)
-	local opts = {
+	local opts_orig = {
 		on_attach = require("user.lsp.handlers").on_attach,
 		capabilities = require("user.lsp.handlers").capabilities,
+	}
+	local opts = {
+		on_attach = opts_orig.on_attach,
+		capabilities = opts_orig.capabilities,
 	}
 
 	 if server.name == "jsonls" then
@@ -26,6 +30,21 @@ lsp_installer.on_server_ready(function(server)
 	 	-- opts = vim.tbl_deep_extend("force", tsserver_opts, opts)
 	 	opts = tsserver_opts
 	 end
+
+	 if server.name == "eslint" then
+        opts.on_attach = function (client, bufnr)
+            -- neovim's LSP client does not currently support dynamic capabilities registration, so we need to set
+            -- the resolved capabilities of the eslint server ourselves!
+            client.resolved_capabilities.document_formatting = true
+            opts_orig.on_attach(client, bufnr)
+        end
+        opts.settings = {
+            format = { enable = true }, -- this will enable formatting
+        }
+
+        -- vim.cmd("autocmd BufWritePre <buffer> EslintFixAll")
+    end
+
 	-- This setup() function is exactly the same as lspconfig's setup function.
 	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 	server:setup(opts)
--- todo.md
+++ todo.md
@@ -4,6 +4,8 @@
   - [x] typescript
   - [ ] html, css, tailwind
   - [x] null-ls eslint
+    - error with fizzbuzz
+  - [ ] how to insert a copilot complete block
 - [x] look into dialog, like hover type
 - [ ] elixir
 - [ ] rails
