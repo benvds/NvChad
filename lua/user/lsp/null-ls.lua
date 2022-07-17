a83b3950237a11b6d673f973f52c2b8b45381987
Ben van de Sande <ben@pomodoro.nl>
2022-07-17 20:02:51 +0200

    update credo ls config

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -1,7 +1,11 @@
+local util = require("lspconfig/util")
 local null_ls = require("null-ls")
 
 null_ls.setup({
     sources = {
-        null_ls.builtins.diagnostics.credo
+        null_ls.builtins.diagnostics.credo.with({
+            extra_filetypes = { "eelixir", "heex" },
+            root_dir = util.root_pattern(".credo.exs", "mix.exs", ".env"),
+        })
     },
 })
